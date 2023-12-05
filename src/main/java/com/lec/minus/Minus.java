d19cd8706961a6b6cd45edcacffaefe4cf186f76
shin-na-ra <nas221@naver.com>
2023-12-05 18:12:00 +0900

    2023.12.05 : 세 정수 뺄셈 코드

--- src/main/java/com/lec/minus/Minus.java
+++ src/main/java/com/lec/minus/Minus.java
@@ -2,4 +2,14 @@ package com.lec.minus;
 
 public class Minus {
 
+	public Minus() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int substraCalc(int num1, int num2, int num3) {
+		int result = 0;
+		result = num1 - num2 - num3;
+		
+		return result;
+	}
 }
