a893c0caae77ae6f6aa3238820289dbfdf94f18e
shin-na-ra <nas221@naver.com>
2023-12-05 18:03:19 +0900

    2023.12.05 shin-add-patch1 : 세 개 정수 합 구하기

--- src/main/java/com/lec/add/Add.java
+++ src/main/java/com/lec/add/Add.java
@@ -2,4 +2,14 @@ package com.lec.add;
 
 public class Add {
 
+	public Add() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int addCalc(int num1, int num2, int num3) {
+		int sum = 0;
+		sum = num1 + num2 + num3;
+		
+		return sum;
+	}
 }
