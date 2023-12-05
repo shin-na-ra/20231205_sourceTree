7e1b98129c0c76116641207dc9054bd0e114ff4c
shin-na-ra <nas221@naver.com>
2023-12-05 18:06:35 +0900

    2023.12.05 : 세개 정수 곱하기

--- src/main/java/com/lec/Multi/Multi.java
+++ src/main/java/com/lec/Multi/Multi.java
@@ -2,4 +2,14 @@ package com.lec.Multi;
 
 public class Multi {
 
+	public Multi() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int multiCalc(int num1 , int num2, int num3) {
+		int multi = 0;
+		multi = num1 * num2 * num3;
+		
+		return multi;
+	}
 }
