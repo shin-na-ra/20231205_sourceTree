1e43454ade3d878d698a39b475db1cb2f7846f58
shin-na-ra <nas221@naver.com>
2023-12-05 18:14:15 +0900

    2023.12.05 : 세정수 나눗셈 코드

--- src/main/java/com/lec/divide/Divide.java
+++ src/main/java/com/lec/divide/Divide.java
@@ -2,4 +2,14 @@ package com.lec.divide;
 
 public class Divide {
 
+	public Divide() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int divCalc(int num1, int num2, int num3) {
+		int result = 0;
+		result = (num1 / num2) / num3;
+		
+		return result;
+	}
 }
